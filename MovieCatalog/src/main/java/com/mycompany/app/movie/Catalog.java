864545c6d968776a62bb6ee504b206734a531171
Galanti <n0254853@lm.lmig.com>
2018-10-08 09:17:20 -0700

    Fixed minor formatting.

--- MovieCatalog/src/main/java/com/mycompany/app/movie/Catalog.java
+++ MovieCatalog/src/main/java/com/mycompany/app/movie/Catalog.java
@@ -1,34 +1,34 @@
 package com.mycompany.app.movie;
 
-	import java.util.ArrayList;
-	import java.util.List;
-	
-	public class Catalog {
-		private final List<Movie> store = new ArrayList<Movie>();
-	
-		public void addMovie(final Movie movie) {
-			store.add(movie);
-		}
-		
-		public List<Movie> findMovieByKeyword(final String keyword) {
-			List<Movie> result = new ArrayList<Movie>();
-			
-			for (Movie movie : store) {
-				if (movie.getTitle().toLowerCase().contains(keyword.toLowerCase())) {
-					result.add(movie);
-				}
-			}
-			
-			return result;
-		}
-		
-		public List<Movie> findMovieByOrder(int index) {
-			List<Movie> result = new ArrayList<Movie>();
-			result.add(store.get(index));
-			return result;
-		}
-		
-		public int getSize() {
-			return store.size();
-		}
+import java.util.ArrayList;
+import java.util.List;
+
+public class Catalog {
+	private final List<Movie> store = new ArrayList<Movie>();
+
+	public void addMovie(final Movie movie) {
+		store.add(movie);
 	}
+
+	public List<Movie> findMovieByKeyword(final String keyword) {
+		List<Movie> result = new ArrayList<Movie>();
+
+		for (Movie movie : store) {
+			if (movie.getTitle().toLowerCase().contains(keyword.toLowerCase())) {
+				result.add(movie);
+			}
+		}
+
+		return result;
+	}
+
+	public List<Movie> findMovieByOrder(int index) {
+		List<Movie> result = new ArrayList<Movie>();
+		result.add(store.get(index));
+		return result;
+	}
+
+	public int getSize() {
+		return store.size();
+	}
+}
--- MovieCatalog/src/test/resources/feature/movie/search_movie.feature
+++ MovieCatalog/src/test/resources/feature/movie/search_movie.feature
@@ -2,9 +2,9 @@
 Feature: Movie Search
   To allow a user to search through a movie catalog and find a desired movie.
 
-	Background:
-		Given a movie with the title 'Inside Out', produced by 'Pixar', released 19 June 2015
-		And a movie with the title 'Out of Africa', produced by 'Universal Pictures', released 20 December 1985
+  Background:
+    Given a movie with the title 'Inside Out', produced by 'Pixar', released 19 June 2015
+	And a movie with the title 'Out of Africa', produced by 'Universal Pictures', released 20 December 1985
     And a movie with the title 'Avatar', produced by '20th Century Fox', released 18 December 2009
     
   @tag1
@@ -16,12 +16,12 @@ Feature: Movie Search
     Then <num_mov> movie should have been found
     And the catalog should have <tot_mov> total movies
     
-		Examples:
-		|	keyword |	num_mov |	tot_mov |
+	Examples:
+	|	keyword | num_mov |	tot_mov |
     |	'Out'   | 2       | 3       |
     	
     
-	@tag1
+  @tag1
   Scenario: Search movies by order
     #Given a movie with the title 'Inside Out', produced by 'Pixar', released 19 June 2015
     #And a movie with the title 'Out of Africa', produced by 'Universal Pictures', released 20 December 1985
